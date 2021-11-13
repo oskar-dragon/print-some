d26bd231651f8e3fb085876bbd2268b667053c68
Oskar Dragon <oskartdragon@gmail.com>
2021-11-13 21:58:00 +0000

    Refactored code

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from "react";
+import Header from "./components/Header";
 import { Routes, Route } from "react-router-dom";
 
-import Header from "./components/Header";
 import Cart from "./pages/Cart";
 import Photos from "./pages/Photos";
 
